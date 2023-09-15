63328f8fb02ce32ed0b65fa368b98f7e7140a45b
Dayarat <kavindusandaruwan703@gmail.com>
2023-09-16 00:58:11 +0530

    connect monogodb database

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
+const { connect } = require("mongoose");
 const app = require("./app");
-
+const connectDatabase = require("./db/Database.js");
 
 
 process.on("uncaughtException", (err) => {
@@ -12,7 +13,20 @@ if(process.env.NODE_ENV !== "PRODUCTION")
         path:"backend/config/.env"
 })
 
+//connect db
+connectDatabase();
 
 const server = app.listen(process.env.PORT,() => {
-    console.log(`Server is running on http://localhost:${process.eventNames.PORT}`)
+    console.log(`Server is running on http://localhost:${process.env.PORT}`)
 })
+
+
+//unhandled promise rejection
+process.on("unhandledRejection", (err)=> {
+    console.log(`Shutting down the server for ${err.message}`);
+    console.log(`Shutting down the server for unhandle promise rejection`);
+
+    server.close(()=>{
+        process.exit(1);
+    })
+});
