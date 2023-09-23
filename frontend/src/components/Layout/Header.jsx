8cf2232019558d613b3eef73ac2689157003501a
Dayarat <kavindusandaruwan703@gmail.com>
2023-09-23 23:48:02 +0530

    set header frontend

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -1,20 +1,22 @@
 import { Link } from "react-router-dom"
 import styles from "../../styles/styles"
-import {productData} from "../../static/data"
+import { categoriesData, productData} from "../../static/data"
 import React, { useState } from 'react';
-
-import {
-  AiOutlineHeart,
-  AiOutlineSearch,
-  AiOutlineShoppingCart,
-} from "react-icons/ai";
+import { AiOutlineHeart, AiOutlineSearch, AiOutlineShoppingCart } from "react-icons/ai";
+import {IoIosArrowDown,IoIosArrowForward} from "react-icons/io";
+import { BiMenuAltLeft } from "react-icons/bi";
+import DropDown from "./DropDown";
+import Navbar from "./Navbar";
+import {CgProfile} from "react-icons/cg";
 
 // Your component code here
 
 
-const Header = () => {
+const Header = ({activeHeading}) => {
   const [searchTerm,setSearchTerm] = useState("");
   const [searchData,setSearchData] = useState(null);
+  const [active,setActive] = useState(false);
+  const [dropDown,setDropDown] = useState(false);
 
   const handleSearchChange = (e) => {
     const term = e.target.value;
@@ -24,11 +26,20 @@ const Header = () => {
       product.name.toLowerCase().includes(term.toLowerCase())
     );
     setSearchData(filteredProducts);
-    console.log("Filtered Products:", filteredProducts);
-  }
-  console.log("Search Data:", searchData);
+    
+  };
+   
+  window.addEventListener("scroll", () => {
+    if(window.screenY > 70){
+      setActive(true);
+    }else{
+      setActive(false);
+    }
+  });
+  
 
   return (
+    <>
     <div className={`${styles.section}`}>
       <div className="hidden 800px:h-[50px] 800px:my-[20px] 800px:flex items-center justify-between">
       <div>
@@ -51,13 +62,13 @@ const Header = () => {
 
                   const Product_name = d.replace(/\s+/g, "-");
 
-                  const imageUrl = i.images && i.images[0]?.url;
+                  // const imageUrl = i.images && i.images[0]?.url;
                   
                     return (
-                      <Link to={`/product/${i._id}`}>
+                      <Link to={`/product/${Product_name}`}>
                         <div className="w-full flex items-start-py-3">
                           <img
-                            src={imageUrl}
+                            src={i.image_Url[0].url}
                             alt=""
                             className="w-[40px] h-[40px] mr-[10px]"
                           />
@@ -69,9 +80,83 @@ const Header = () => {
               </div>
             ) : null}
         </div>
+          <div className={`${styles.button}`}>
+              <Link to= "/seller">
+                <h1 className="text-[#fff] flex items-center">
+                  Become Seller <IoIosArrowForward className="ml-1"/>
+                </h1>
+              </Link>
+          </div>
+
+      </div>
+      <div
+        className={`${
+          active === true ? "shadow-sm fixed top-0 left-0 z-10" : null
+        } transition hidden 800px:flex items-center justify-between w-full bg-[#3321c8] h-[70px]`}
+      >
+        <div
+          className={`${styles.section} relative ${styles.noramlFlex} justify-between`}
+        >{/* categories */}
+        <div onClick={() => setDropDown(!dropDown)}>
+          <div className="relative h-[60px] mt-[10px] w-[270px] hidden 1000px:block">
+            <BiMenuAltLeft size={30} className="absolute top-3 left-2" />
+            <button
+              className={`h-[100%] w-full flex justify-between items-center pl-10 bg-white font-sans text-lg font-[500] select-none rounded-t-md`}
+            >
+              All Categories
+            </button>
+            <IoIosArrowDown
+                size={20}
+                className="absolute right-2 top-4 cursor-pointer"
+                onClick={() => setDropDown(!dropDown)}
+              />
+              {dropDown ? (
+                <DropDown
+                  categoriesData={categoriesData}
+                  setDropDown={setDropDown}
+                />
+              ) : null}
+              
+            </div>
+            </div>
+            {/* navbar*/}
+            <div className={`${styles.normalFlex}`}>
+                <Navbar active={activeHeading}/>
+              </div>
+        </div>
+        <div className="flex">
+                <div className={`${styles.normalFlex}`}>
+                  <div className="relative cursor-pointer mr-[15px]">
+                    <AiOutlineHeart size={30} color="rgb(255 255 255 /83%)"/>
+                    <span className="absolute right-0 top-0 rounded-full bg-[#3bc177] w-4 h-4 top right p-0 m-0 text-white font-mono text-[12px] leading-tight text-center">
+                      0
+                    </span>
+                  </div>
+
+                </div>
+                <div className={`${styles.normalFlex}`}>
+                  <div className="relative cursor-pointer mr-[15px]">
+                    <AiOutlineShoppingCart size={30} color="rgb(255 255 255 /83%)"/>
+                    <span className="absolute right-0 top-0 rounded-full bg-[#3bc177] w-4 h-4 top right p-0 m-0 text-white font-mono text-[12px] leading-tight text-center">
+                      1
+                    </span>
+                  </div>
+
+                </div>
+                <div className={`${styles.normalFlex}`}>
+                  <div className="relative cursor-pointer mr-[15px]">
+                    <Link to="/login">
+                    <CgProfile size={30} color="rgb(255 255 255 /83%)"/>
+
+                    </Link>
+                  </div>
+
+                </div>
+          </div>
       </div>
     </div>
-  )
-}
+    </>
+  );
+};
 
 export default Header
